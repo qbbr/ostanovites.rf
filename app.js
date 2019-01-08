c0f5d4487ba34abdc6b5fb8cb0eab7909f667bfa
Sokolov Innokenty <im@qbbr.io>
2019-01-08 17:19:53 +0800

    added thief-face.png with border radius 100%

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 new Vue({
     el: '#app',
     data: {
-        thiefFaceImg: '/thief-face.jpeg',
+        thiefFaceImg: '/thief-face.png',
         headerText: 'Остановитесь',
         myTelegramLink: 'https://t.me/imqbbr',
         soundPath: '/ostanovites.mp3',
