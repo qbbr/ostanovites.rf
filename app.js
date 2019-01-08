abe052d7d7d8d8862d2cbaa969394a5cc1864b5c
Sokolov Innokenty <im@qbbr.io>
2019-01-08 17:05:30 +0800

    rewrite app on Vue.js

---
+++ app.js
@@ -0,0 +1,40 @@
+new Vue({
+    el: '#app',
+    data: {
+        thiefFaceImg: '/thief-face.jpeg',
+        headerText: 'Остановитесь',
+        myTelegramLink: 'https://t.me/imqbbr',
+        soundPath: '/ostanovites.mp3',
+        fontSize: 28,
+        fontSizeIncrement: 5
+    },
+    methods: {
+        printConsoleHeader: function () {
+            console.log(' %c!!! %c' + this.headerText + ' %c!!!', 'font-size: 3em; color: #550000;', 'font-size: 3em; color: #990000;', 'font-size: 3em; color: #330000;');
+        },
+        click: function () {
+            this.fontSize += this.fontSizeIncrement;
+            this.$refs.header.style.fontSize = this.fontSize + 'px';
+
+            this.printConsoleHeader();
+            this.playSound(this.soundPath);
+        },
+        playSound: function (sound) {
+            var audio = new Audio(sound);
+            audio.pause();
+            audio.currentTime = 0;
+            audio.playbackRate = 0.85;
+            audio.play();
+        },
+    },
+    mounted: function () {
+        this.printConsoleHeader();
+    },
+    template: [
+        '<div>',
+            '<h1 ref="header">{{ headerText }}</h1>',
+            '<img :src="thiefFaceImg" @click="click">',
+            '<h6>created by <a :href="myTelegramLink">@qbbr</a></h6>',
+        '</div>'
+    ].join('\n')
+});
