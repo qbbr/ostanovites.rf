58cde6b7a8c7864ab89c78717bb68caf2e84c755
Sokolov Innokenty <im@qbbr.io>
2021-09-21 12:45:44 +0800

    welcome to the hell...

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ new Vue({
     el: '#app',
     data: {
         thiefFaceImg: '/thief-face.jpeg',
-        headerText: 'Остановитесь',
+        headerText: 'Остановитесь!1!',
         myTelegramLink: 'https://t.me/imqbbr',
         soundPath: '/ostanovites.mp3',
         fontSize: 28,
-        fontSizeIncrement: 5
+        fontSizeIncrement: 5,
+        hellCounter: 0,
+        hell: false
     },
     methods: {
         printConsoleHeader: function () {
             console.log(' %c!!! %c' + this.headerText + ' %c!!!', 'font-size: 3em; color: #550000;', 'font-size: 3em; color: #990000;', 'font-size: 3em; color: #330000;');
         },
-        click: function () {
+        click: function (self) {
             this.fontSize += this.fontSizeIncrement;
             this.$refs.header.style.fontSize = this.fontSize + 'px';
 
             this.printConsoleHeader();
             this.playSound(this.soundPath);
+
+            ++this.hellCounter;
+            if (this.hellCounter > (6 + Math.floor(Math.random() * 6))) { // 6...
+                this.hell = true;
+            }
         },
         playSound: function (sound) {
             var audio = new Audio(sound);
@@ -33,7 +40,7 @@ new Vue({
     template: [
         '<div>',
             '<h1 ref="header">{{ headerText }}</h1>',
-            '<img :src="thiefFaceImg" @click="click">',
+            '<img :src="thiefFaceImg" :class="{ hell: hell }" @click="click()">',
             '<h6>with ❤ by <a :href="myTelegramLink">@qbbr</a></h6>',
         '</div>'
     ].join('\n')
